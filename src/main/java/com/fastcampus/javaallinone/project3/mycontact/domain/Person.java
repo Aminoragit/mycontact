79db779eda6ecb2551ea8828e9755cc02a7dc9b5
Aminoragit <wjsxkxk1@gmail.com>
2021-04-16 14:48:30 +0900

    03.JPA Lombok2-2

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -8,14 +8,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@Entity
-@NoArgsConstructor //아무요소가 없는 생성자
-@AllArgsConstructor //모든 요소가 포함된 생성자
-@RequiredArgsConstructor //Notnull 이여야하는 요소들 @Nonnull
-@ToString //요소 추가시 자동으로 print 해주는 것에 추가해줌
+//@RequiredArgsConstructor //Notnull 이여야하는 요소들 @Nonnull
+//@ToString //요소 추가시 자동으로 print 해주는 것에 추가해줌
 //exclude는 print되는 요소들중 숨기고 싶은 요소가 있을시 추가해주면 해당 요소는 print 되지 않는다.
+//@EqualsAndHashCode
+
+
+@Entity
+@Data //getter setter Tostring,Requied..., Eqals...를 모두 포함함
+@NoArgsConstructor //아무요소가 없는 생성자
+@RequiredArgsConstructor
+@AllArgsConstructor
+//모든 요소가 포함된 생성자
 public class Person {
     @Id
     @GeneratedValue
@@ -26,7 +30,10 @@ public class Person {
 
     @NonNull
     private int age;
+
     private String hobby;
+
+    @NonNull
     private String bloodType;
     private String address;
     private LocalDate birthday;
@@ -35,19 +42,25 @@ public class Person {
     @ToString.Exclude
     private String phoneNumber;
 
-    public boolean equals(Object object) {
-        if (object == null) {
-            return false;
-        }
-        Person person = (Person) object;
 
-        if (!person.getName().equals(this.getName())) {
-            return false;
-        }
 
-        if (person.getAge() != this.getAge()) {
-            return false;
-        }
-        return true;
-    }
+//    public boolean equals(Object object) {
+//        if (object == null) {
+//            return false;
+//        }
+//        Person person = (Person) object;
+//
+//        if (!person.getName().equals(this.getName())) {
+//            return false;
+//        }
+//
+//        if (person.getAge() != this.getAge()) {
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    public int hashCode(){
+//        return (name+age).hashCode();
+//    }
 }
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersonRepositoryTest {
@@ -35,12 +35,22 @@ class PersonRepositoryTest {
 
    @Test
     void hashCodeAndEquals(){
-       Person person1= new Person("martin",10);
-       Person person2= new Person("martin",10);
+       Person person1= new Person("martin",10,"A");
+       Person person2= new Person("martin",10,"A");
 
        System.out.println(person1.equals(person2)); //Person에서 입력한 equals에 의해 값이 동일한지 확인
        System.out.println(person1.hashCode()); //값은 동일하지만 hashcode는 다르다는걸 알수 있다.
        System.out.println(person2.hashCode());
 
-   }
+
+
+       //왜 hashCode를 통해 가져오면 안되지?-> DB를 호출할때마다 변경되는 HashCode는 불안정하다
+       //따라서 값을 통해 판단하는것이 좋다.
+       Map<Person,Integer> map =new HashMap<>();
+       map.put(person1,person1.getAge());
+
+       System.out.println(map);
+       System.out.println(map.get(person2));
+
+    }
 }
