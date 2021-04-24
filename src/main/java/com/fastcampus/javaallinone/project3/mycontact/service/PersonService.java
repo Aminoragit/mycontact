a349813ffe0871c18a6bc46435e52b0d43dfd77e
Aminoragit <wjsxkxk1@gmail.com>
2021-04-24 16:22:44 +0900

    Chapter 9 Exception 01

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -2,6 +2,8 @@ package com.fastcampus.javaallinone.project3.mycontact.service;
 
 import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import com.fastcampus.javaallinone.project3.mycontact.exception.PersonNotFoundException;
+import com.fastcampus.javaallinone.project3.mycontact.exception.RenameNotPermittedException;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,10 +47,10 @@ public class PersonService {
 
     @Transactional
     public void modify(Long id, PersonDto personDto) {
-        Person person = personRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
+        Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
 
         if (!person.getName().equals(personDto.getName())) {
-            throw new RuntimeException("이름이 다릅니다.");
+            throw new RenameNotPermittedException();
         }
 
         person.set(personDto);
@@ -56,9 +58,13 @@ public class PersonService {
         personRepository.save(person);
     }
 
+
+    //커스텀 Exception PersonNotFoundException으로 runtimeException을 기반으로 한 좀더 세분화된 exception을 만들수 있다.
     @Transactional
     public void modify(Long id, String name) {
-        Person person = personRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
+        Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+
+        //        Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException("아이디가 존재하지 않습니다."));
         person.setName(name);
         personRepository.save(person);
     }
@@ -66,7 +72,7 @@ public class PersonService {
     @Transactional
     public void delete(Long id) {
         //잘못된 request로 바로 delete하면 DB가 손상될수 있으니 deleted로 확인하고 db에 업로드하는 방식을 사용한다.
-        Person person = personRepository.findById(id).orElseThrow(() -> new RuntimeException("아이디가 존재하지 않습니다."));
+        Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
         person.setDeleted(true);
         personRepository.save(person);
     }
