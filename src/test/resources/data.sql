c207b071e3803233bec864649287a1f426b90b3b
Aminoragit <wjsxkxk1@gmail.com>
2021-04-24 20:23:55 +0900

    과제 data.sql에 목록 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,9 +1,12 @@
-insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(1,'martin',1991,8,15);
-insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(2,'david',1992,7,21);
-insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(3,'dennis',1993,10,15);
-insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(4,'sophia',1994,8,31);
-insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(5,'benny',1995,12,23);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(1,'martin',2021,8,15);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(2,'david',2021,7,21);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(3,'dennis',2021,10,15);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(4,'sophia',2021,8,31);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(5,'benny',2021,12,23);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(6,'hans',2021,4,24);
+insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`) values(7,'hans2',2021,4,25);
+
 insert into person(`id`,`name`,`year_of_birthday`,`month_of_birthday`,`day_of_birthday`,`job`,`hobby`,`phone_number`,`address`)
-    values(6,'tony',1991,7,10,'officer','reading','010-2222-5555','서울');
-insert into person(`id`,`name`,`deleted`) values(7,'andrew',true);
+    values(8,'tony',2021,7,10,'officer','reading','010-2222-5555','서울');
+insert into person(`id`,`name`,`deleted`) values(9,'andrew',true);
 
